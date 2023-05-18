f25b6cacd960192a7aad092eddabe20befabd781
mteruel28 <miguel.teruel98@gmail.com>
2023-05-18 17:09:09 -0500

    New Commits for CSS

--- newbar/src/App.js
+++ newbar/src/App.js
@@ -5,41 +5,59 @@ function App() {
   return (
     <div id="root">
     <div>
-      <div class="container">
+      <div className="container">
         <div class="item-1-header"> <h1>Miguel's Bar</h1></div>
       </div>
-      <div class="search-bar-container">
-        <input class="search-bar" placeholder="search..." type="text"></input>
+      <div className="search-bar-container">
+        <input class="search-bar" placeholder="Search..." type="text"></input>
       </div>
-      <div class="container-2">
-          <div class="card">
-              <h3>About</h3>
-              <p>bonk...</p>
-          </div>
-          <div class="card"> 
-              <h3>Check-out</h3>
-              <p>bonk...</p>
-          </div>
-          <div class="card">
-              <h3>Wines</h3>
-              <p>bonk...</p>
-          </div>
-          <div class="card"> 
-              <h3>Spirits</h3>
-              <p>bonk...</p>
-          </div>
-          <div class="card"> 
-              <h3>Beers & Seltzers</h3>
-              <p>bonk...</p>
-          </div>
-          <div class="card"> 
-              <h3>Mixers & Misc</h3>
-              <p>bonk...</p>
-          </div>
-      </div>
+      <Card/>
     </div>
     </div>
   );
 }
 
+function Card(){
+  return(
+    <div id="root">
+<div className="container-2">
+          <div className="card">
+              <h3 className= "card__title">Rewards</h3>
+              <p className="card_description">This page contains sign information for our rewards program</p>
+              <button className="card__btn"> View Rewards</button>
+          </div>
+         
+          <div className="card"> 
+              <h3 className= "card__title">Shopping Cart</h3>
+              <p className="card_description">This pages features your checkout information</p>
+              <button className="card__btn"> View Shopping Cart</button>
+          </div>
+          <div className="card">
+              <h3 className= "card__title"> Wines</h3>
+              <p className="card_description">This page contains all information about Wines</p>
+              <button className="card__btn"> View Wines</button>
+          </div>
+          <div className="card"> 
+              <h3 className= "card__title">Spirits</h3>
+              <p className="card_description" >This page contains all information about Spirits</p>
+              <button className="card__btn"> View Spirits</button>
+          </div>
+          <div className="card"> 
+              <h3 className= "card__title">Beers & Seltzers</h3>
+              <p className="card_description">This page contains all information about Beers & Seltzers</p>
+              <button className="card__btn"> View Beers & Seltzers</button>
+          </div>
+          <div className="card"> 
+              <h3 className= "card__title">Mixers & Misc</h3>
+              <p className="card_description">This page contains information about Mixers & all other Misc products</p>
+              <button className="card__btn"> View Rewards</button>
+              </div>
+      </div>
+    </div>
+  )
+}
+
 export default App;
+
+
+
