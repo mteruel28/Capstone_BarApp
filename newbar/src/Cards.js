23b875de50954db1c452f4ee60dbb8b57fe5e9e9
mteruel28 <miguel.teruel98@gmail.com>
2023-06-03 16:16:26 -0500

    Adding Images to Cards

--- newbar/src/Cards.js
+++ newbar/src/Cards.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import doge from './img/dogecrazy.jpg';
+import cart from './img/Shoppingcart.jpg';
+import wine from './img/Wines.jpg';
 
 function Cards(){
 
@@ -9,24 +11,41 @@ function Cards(){
             title: "Rewards",
             sub_header: "This page contains sign information for our rewards program",
             link_text: "View Rewards",
-            path: "/rewards",
+            path: "/Rewards",
             img_path: doge
         },
         {
             title: "Shopping Cart",
             sub_header: "This pages features your checkout information",
             link_text: "View Shopping Cart",
-            path: "/rewards"
+            path: "/Shoppingcart",
+            img_path: cart
         },
         {
-            title: "Rewards",
-            sub_header: "This page contains sign information for our rewards program",
-            link_text: "View Rewards",
-            path: "/rewards"
+            title: "Wines",
+            sub_header: "This page contains all information about Wines",
+            link_text: "View Wines",
+            path: "/Wines" ,
+            img_path: wine
             
         },
         {
-
+            title: "Spirits",
+            sub_header: "This page contains all information about Spirits",
+            link_text: "View Spirits",
+            path: "/Spirits"
+        },
+        {
+            title: "Beers & Seltzers",
+            sub_header: "This page contains all information about Beers & Seltzers",
+            link_text: "View Beers & Seltzers",
+            path: "/BeersAndSeltzers"
+        },
+        {
+            title: "Mixers & Misc",
+            sub_header: "This page contains information about Mixers & all other Misc products",
+            link_text: "View Mixers & Misc",
+            path: "/MixersAndMisc"
         }
     ]
 
@@ -41,7 +60,7 @@ function Cards(){
                         <h3 className= "card__title">{card.title}</h3>
                         <p className="card_description">{card.sub_header}</p>
                         <Link to={card.path}>{card.link_text}</Link>
-                        <img src={card.img_path} width='50' height='50' />
+                        <img src={card.img_path} width='100' height='100' />
                     </div>
                 ))
             }
