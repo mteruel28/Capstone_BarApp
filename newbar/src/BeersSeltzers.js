0e433d33ce7445e029e13bbe9a348fc6b8e18a5a
mteruel28 <miguel.teruel98@gmail.com>
2023-06-14 01:43:55 -0500

    New Changes CSS

--- newbar/src/BeersSeltzers.js
+++ newbar/src/BeersSeltzers.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ItemCards from './ItemCards';
-
+import './Beer.css';
 
 function BeersSelters(){
     return(
         <div>
        <p>Beers and Seltzers</p>
+       <div className="search-bar-container">
+                <input className="search-bar" placeholder="Search..." type="text"></input>
+            </div>
        <ItemCards/>
        </div>
     )
