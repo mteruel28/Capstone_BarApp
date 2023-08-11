93660c90593827a8997a9ca855b68972d8d49ed9
mteruel28 <miguel.teruel98@gmail.com>
2023-08-11 00:46:27 -0500

    edit the rewards page, still needs design

--- newbar/src/Rewards.js
+++ newbar/src/Rewards.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Rewards(){
     return(
-       <p>Bonk</p>
+       <p>This Page is supposed to elicit a Rewards page , which keeps track of points customers receive upon ordering alcohol.</p>
     )
 }
 export default Rewards;
